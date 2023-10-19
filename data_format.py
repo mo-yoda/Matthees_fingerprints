3b0a2f23d941d4cfe4ab9f7a01d095815a2a8938
mo-yoda <monareichel.mr@gmail.com>
2023-10-19 16:10:21 +0200

    edited path and xlsx file name to import

--- data_format.py
+++ data_format.py
@@ -85,10 +85,10 @@ def dic_to_table(dic):
     return reformatted_data
 
 # Path definition
-path_to_file = "C:/Users/monar/Google Drive/Arbeit/homeoffice/230918_EM_PROGRAM/"
+path_to_file = "C:/Users/monar/Google Drive/Arbeit/homeoffice/231119_EM_PROGRAM_newdata/"
 
 # Read the entire Excel workbook
-all_data = pd.read_excel(f"{path_to_file}/230913_overview_b2,b2V2,V2b2,V2_bArrs-confChange_prepR.xlsx",
+all_data = pd.read_excel(f"{path_to_file}/231012_overview_b2,b2V2,V2b2,V2_bArrs-confChange_prepR.xlsx",
                          sheet_name=None, nrows=7)
 # imported as dictionary where:
 #   Key: Sheet name
