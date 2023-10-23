f563954807912a806ba333a07dc87bd87fee1e4b
mo-yoda <monareichel.mr@gmail.com>
2023-10-23 15:31:00 +0200

    Master data is processed and filtered in preparation of matrix for heatmap

--- responsive_sensors_categorization.R
+++ responsive_sensors_categorization.R
@@ -1,5 +1,6 @@
 ###create needed environment with loading packages
 wants <- c("openxlsx",
+            "dplyr",
            "writexl"
 )
 has <- wants %in% rownames(installed.packages())
@@ -56,3 +57,61 @@ write_xlsx(notfiltered_but_notconcdep, "NotFiltered_but_NotconcDep.xlsx")
 big_HS <- modified_data[modified_data$Hill_slope > 1,]
 summary(as.factor(big_HS$FlAsH))
 # no specific F position with higher HS
+
+### Data processing ###
+# tower PC path
+path <- r"(C:\Users\monar\Google Drive\Arbeit\homeoffice\231119_EM_PROGRAM_newdata)"
+# laptop path
+path <- r"(C:\Users\marli\Desktop\231119_EM_PROGRAM_newdata)"
+setwd(path)
+
+# Load data
+import_file <- "Master_SN_reformat.xlsx"
+data <- readxl::read_xlsx(import_file)
+
+include_data <- modified_data[
+  modified_data$`HS_gt_-1_EC50_btwn_-2.5_and_0.3`,]
+
+set_unmatched_to_zero <- function(data, reference) {
+  # Create 'experiment' column for both data and reference dfs
+  data$experiment <- paste(data$GPCR, data$bArr, data$cell_background, data$FlAsH, sep = "_")
+  reference$experiment <- paste(reference$GPCR, reference$bArr, reference$cell_background, reference$FlAsH, sep = "_")
+
+  # Identify rows in 'data' that are not found in 'reference'
+  unmatched_data <- dplyr::anti_join(data, reference, by = "experiment")
+
+  print("---- No. experiments set to 0 (non-responsive) ----")
+  print(length(unmatched_data$experiment))
+
+  # If there are unmatched rows, set their mean_signal to 0
+  if (nrow(unmatched_data) > 0) {
+    data[data$experiment %in% unmatched_data$experiment, "mean_signal"] <- 0
+  }
+  return(data)
+}
+
+prepare_matrix_data <- function(unprocessed_data, reference) {
+  # Filter data for ligand_conc == 1
+  subset_data <- unprocessed_data[unprocessed_data$ligand_conc == 1,]
+
+  # Group by the combination of factors and filter groups with 3 or more values
+  grouped_data <- subset_data %>%
+    dplyr::group_by(GPCR, bArr, cell_background, FlAsH)
+
+  # Calculate the mean of the signal for each group
+  result <- grouped_data %>%
+    dplyr::summarise(mean_signal = mean(signal, na.rm = TRUE))
+
+  print("###### No. of experiments measured #########")
+  print(length(result$GPCR))
+
+  # Set mean_signal to 0 for non-responsive conditions
+  result <- set_unmatched_to_zero(result, reference)
+
+  return(result)
+}
+
+processed_data <- prepare_matrix_data(data, include_data)
+
+
+
