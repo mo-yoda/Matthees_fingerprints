35d87b4edc06478adebfdb0f795b1dc7e8085afa
mo-yoda <monareichel.mr@gmail.com>
2023-10-23 16:25:26 +0200

    function to draw heatmap

--- heatmap_generation.R
+++ heatmap_generation.R
@@ -2,6 +2,7 @@
 wants <- c("openxlsx",
            "dplyr",
            "tidyr",
+           "pheatmap",
            "writexl"
 )
 has <- wants %in% rownames(installed.packages())
@@ -19,13 +20,11 @@ setwd(path)
 import_file <- "Filtered_SN_Master.xlsx"
 filtered_data <- readxl::read_xlsx(import_file)
 
-# Create matrix for heatmap
-
-# Helper function to create the matrix
+# Create matrix for heatmap (optional subsetting of several levels)
 create_matrix_from_factors <- function(data, col_factor,
                                        subset_factor = NULL,
                                        subset_levels = NULL) {
- # Check if a subset is requested
+  # Check if a subset is requested
   if (!is.null(subset_factor) && !is.null(subset_levels)) {
     data <- dplyr::filter(data, !!sym(subset_factor) %in% subset_levels)
   }
@@ -50,6 +49,47 @@ create_matrix_from_factors <- function(data, col_factor,
 
 }
 
+draw_heatmap <- function(matrix_data,
+                         clustering_distance_rows = "manhattan",
+                         clustering_distance_cols = "manhattan",
+                         cutree_rows = NA,
+                         cutree_cols = NA,
+                         display_numbers = FALSE,
+                         height = 8,
+                         width = 10,
+                         fontsize = 10) {
+
+  # If clustering distance is defined, then clustering is enabled
+  clustering_rows <- !is.null(clustering_distance_rows)
+  clustering_cols <- !is.null(clustering_distance_cols)
+
+  # If cutree is not specified, set to zero
+  if (is.na(cutree_rows)) cutree_rows <- 1
+  if (is.na(cutree_cols)) cutree_cols <- 1
+
+  # Create a custom annotation bar for the cell numbers
+  annotation = NULL
+  if (display_numbers) {
+    annotation <- matrix_data
+    annotation_colors <- colorRampPalette(c("white", "black"))(length(unique(c(matrix_data))))
+    names(annotation_colors) <- sort(unique(c(matrix_data)))
+  }
+
+  pheatmap(matrix_data,
+           clustering_distance_rows = clustering_distance_rows,
+           clustering_distance_cols = clustering_distance_cols,
+           cutree_rows = cutree_rows,
+           cutree_cols = cutree_cols,
+           cellwidth = width,
+           cellheight = height,
+           fontsize = fontsize,
+           display_numbers = display_numbers,
+           cluster_rows = clustering_rows,
+           cluster_cols = clustering_cols,
+           border_color = NA)
+}
+
+
 m1 <- create_matrix_from_factors(filtered_data, "FlAsH")
 
 m2 <- create_matrix_from_factors(filtered_data,
@@ -57,4 +97,4 @@ m2 <- create_matrix_from_factors(filtered_data,
                                  "GPCR",
                                  c("V2R", "V2b2"))
 
-
+draw_heatmap(m2)
