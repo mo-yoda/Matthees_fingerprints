0e182cc325e6daf02ce72f82bb71e2c1eac4480c
mo-yoda <monareichel.mr@gmail.com>
2023-10-23 16:09:41 +0200

    function to create matrices as heatmap input

--- heatmap_generation.R
+++ heatmap_generation.R
@@ -1,5 +1,7 @@
 ###create needed environment with loading packages
 wants <- c("openxlsx",
+           "dplyr",
+           "tidyr",
            "writexl"
 )
 has <- wants %in% rownames(installed.packages())
@@ -15,4 +17,44 @@ setwd(path)
 
 # Load data
 import_file <- "Filtered_SN_Master.xlsx"
-data <- readxl::read_xlsx(import_file)
+filtered_data <- readxl::read_xlsx(import_file)
+
+# Create matrix for heatmap
+
+# Helper function to create the matrix
+create_matrix_from_factors <- function(data, col_factor,
+                                       subset_factor = NULL,
+                                       subset_levels = NULL) {
+ # Check if a subset is requested
+  if (!is.null(subset_factor) && !is.null(subset_levels)) {
+    data <- dplyr::filter(data, !!sym(subset_factor) %in% subset_levels)
+  }
+  # Create a combined factor from the factors that aren't the col_factor
+  factors <- c("GPCR", "bArr", "cell_background", "FlAsH")
+  row_factors <- setdiff(factors, col_factor)
+  data$combined_factor <- apply(data[row_factors], 1, paste, collapse = "_")
+
+  # Spread the data to form the matrix
+  matrix_data <- data %>%
+    dplyr::select(combined_factor, !!sym(col_factor), mean_signal) %>%
+    tidyr::spread(!!sym(col_factor), mean_signal)
+
+  # Convert tibble to a standard data.frame
+  matrix_data <- as.data.frame(matrix_data)
+
+  # Set row names
+  rownames(matrix_data) <- matrix_data$combined_factor
+  matrix_data$combined_factor <- NULL  # Remove the 'combined_factor' column
+
+  return(matrix_data)
+
+}
+
+m1 <- create_matrix_from_factors(filtered_data, "FlAsH")
+
+m2 <- create_matrix_from_factors(filtered_data,
+                                 "cell_background",
+                                 "GPCR",
+                                 c("V2R", "V2b2"))
+
+
