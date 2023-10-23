0fae660c82e583a6d5423cc26ab926f0a26ab2ac
mo-yoda <monareichel.mr@gmail.com>
2023-10-23 15:38:14 +0200

    import Master

---
+++ heatmap_generation.R
@@ -0,0 +1,18 @@
+###create needed environment with loading packages
+wants <- c("openxlsx",
+           "writexl"
+)
+has <- wants %in% rownames(installed.packages())
+if (any(!has)) install.packages(wants[!has])
+lapply(wants, require, character.only = TRUE)
+
+### Data processing
+# tower PC path
+path <- r"(C:\Users\monar\Google Drive\Arbeit\homeoffice\231119_EM_PROGRAM_newdata)"
+# laptop path
+path <- r"(C:\Users\marli\Desktop\231119_EM_PROGRAM_newdata)"
+setwd(path)
+
+# Load data
+import_file <- "Filtered_SN_Master.xlsx"
+data <- readxl::read_xlsx(import_file)
