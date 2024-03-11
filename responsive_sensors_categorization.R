f141fe9aebf6d376dbbae004408cd582ef1e6970
mo-yoda <monareichel.mr@gmail.com>
2024-03-11 17:58:12 +0100

    edit filter to use absolute Hill slope

--- responsive_sensors_categorization.R
+++ responsive_sensors_categorization.R
@@ -22,8 +22,8 @@ fit_pars_og <- readxl::read_xlsx("Fit_parameters_classes.xlsx")
 # constrain function
 add_constraint_column <- function(df, hill_lower, ec50_upper, ec50_lower) {
   # Create the logical column
-  col_name <- paste0("HS_gt_", hill_lower, "_EC50_btwn_", ec50_lower, "_and_", ec50_upper)
-  df[[col_name]] <- with(df, Hill_slope > 10^hill_lower &
+  col_name <- paste0("absHS_gt_", hill_lower, "_EC50_btwn_", ec50_lower, "_and_", ec50_upper)
+  df[[col_name]] <- with(df, absolute_Hill_slope > 10^hill_lower &
     EC50 > 10^ec50_lower &
     EC50 < 10^ec50_upper)
 
@@ -31,8 +31,8 @@ add_constraint_column <- function(df, hill_lower, ec50_upper, ec50_lower) {
   return(df)
 }
 
-# experiments with Hill_slope >10^-1 and 10^-3.5> EC50 < 10^1
-modified_data <- add_constraint_column(modified_data, -1, 0.3, -3.0)
+# experiments with absolute_Hill_slope >10^-1 and 10^-3.0 > EC50 < 10^0.3
+modified_data <- add_constraint_column(fit_pars_og, -1, 0.3, -3.0)
 write_xlsx(modified_data, "Fit_parameters_classes_filtered.xlsx")
 
 ### Data processing ###
@@ -47,7 +47,7 @@ import_file <- "Master_SN_reformat.xlsx"
 data <- readxl::read_xlsx(import_file)
 
 include_data <- modified_data[
-  modified_data$`HS_gt_-1_EC50_btwn_-3_and_0.3`,]
+  modified_data$`absHS_gt_-1_EC50_btwn_-3_and_0.3`,]
 
 set_unmatched_to_zero <- function(data, reference) {
   # Create 'experiment' column for both data and reference dfs
