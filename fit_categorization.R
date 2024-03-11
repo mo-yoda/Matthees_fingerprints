dfb5f83853a5c28f22e9edf01f05b41bf6e9a9cb
mo-yoda <monareichel.mr@gmail.com>
2024-03-11 17:21:45 +0100

    REMOVED previous trials: removed optional processing of non_normalised data
    removed boxplots separated by Cterm and untransformed and other factors apart drom conc_dep "unclear" and RMSE/MAE
    
    removed unused 2D plots
    edited 2D plots to use absolute HS

--- fit_categorization.R
+++ fit_categorization.R
@@ -45,12 +45,7 @@ setwd(path)
 
 # Load data
 classes <- readxl::read_xlsx("231012_categorized-master-plot_List.xlsx")
-
-# CHOOSE: normalised or non-normalised data
-path_to_Normfits <- paste0(path, r"(\start_normalised\)")
-path_to_NonNormfits <- paste0(path, r"(\non_normalised\)")
-
-setwd(path_to_Normfits)
+setwd(paste0(path, r"(\start_normalised\)"))
 fit_pars_og <- readxl::read_xlsx("Fit_parameters.xlsx")
 
 # Split the 'experiment' column into separate columns for each factor
@@ -85,9 +80,8 @@ merged_data <- merged_data %>%
     conc_dep = translate_to_logical(conc_dep),
     critical = translate_to_logical(critical)
   )
-folder_name <- c("categories")
-if (!dir.exists(folder_name)) {
-    dir.create(folder_name)
+if (!dir.exists("categories")) {
+    dir.create("categories")
   }
 write_xlsx(merged_data, paste0(getwd(), r"(\categories\Fit_parameters_classes.xlsx)"))
 
@@ -151,13 +145,6 @@ data_V2 <- merged_data %>%
 data_b2 <- merged_data %>%
   filter(str_starts(GPCR, "b2"))
 
-# test Separation based on Cterm
-data_V2Cterm <- merged_data %>%
-  filter(str_detect(GPCR, "b2V2") | str_detect(GPCR, "V2R"))
-
-data_b2Cterm <- merged_data %>%
-  filter(str_detect(GPCR, "b2AR") | str_detect(GPCR, "V2b2"))
-
 # initialize plot_list
 plot_list <- list()
 
@@ -165,170 +152,7 @@ plot_list <- list()
 # Warning: Removed XX rows containing non-finite values (`stat_boxplot()`)
 # -> due to ylim setting!, there are no NAs or Inf values in this dataset
 
-### conc_dep + unclear factors
-# EC50
-add_plot(plot_list,
-         create_boxplot(data_V2, "EC50",
-                        "conc_dep", "unclear",
-                        ylim_range = c(-0.5, 3)),
-         "concDep_uncl_V2")
-# 16x values not displayed as EC50 > 3; all in conc_dep = FALSE
-add_plot(plot_list,
-         create_boxplot(data_b2, "EC50",
-                        "conc_dep", "unclear",
-                        ylim_range = c(-0.5, 10)),
-         "concDep_uncl_b2_A")
-# 8x values not displayed as EC50 > 10; all in conc_dep = FALSE
-add_plot(plot_list,
-         create_boxplot(data_b2, "EC50",
-                        "conc_dep", "unclear",
-                        ylim_range = c(-0.5, 3)),
-         "concDep_uncl_b2_B")
-# 14x values not displayed as EC50 > 3; in all different groups!
-add_plot(plot_list,
-         create_boxplot(data_V2Cterm, "EC50",
-                        "conc_dep", "unclear",
-                        ylim_range = c(-0.5, 10)),
-         "concDep_uncl_V2Cterm")
-add_plot(plot_list,
-         create_boxplot(data_b2Cterm, "EC50",
-                        "conc_dep", "unclear",
-                        ylim_range = c(-0.5, 10)),
-         "concDep_uncl_b2Cterm")
-
-
-# EC50 log transformed
-add_plot(plot_list,
-         create_boxplot(data_V2, "EC50",
-                        "conc_dep", "unclear",
-                        ylim_range = c(-7, 10), log_transform = TRUE),
-         "concDep_uncl_V2_log")
-# 3x values not displayed as log10(EC50) > 10; all in conc_dep = FALSE
-add_plot(plot_list,
-         create_boxplot(data_b2, "EC50",
-                        "conc_dep", "unclear",
-                        ylim_range = c(-5, 5), log_transform = TRUE),
-         "concDep_uncl_b2_log")
-# all values displayed
-add_plot(plot_list,
-         create_boxplot(data_V2Cterm, "EC50",
-                        "conc_dep", "unclear",
-                        ylim_range = c(-7, 10), log_transform = TRUE),
-         "concDep_uncl_V2Cterm_log")
-add_plot(plot_list,
-         create_boxplot(data_b2Cterm, "EC50",
-                        "conc_dep", "unclear",
-                        ylim_range = c(-5, 5), log_transform = TRUE),
-         "concDep_uncl_b2Cterm_log")
-
-# remaining parameters
-add_plot(plot_list,
-         create_boxplot(merged_data, "Hill_slope",
-                        "conc_dep", "unclear",
-                        ylim_range = c(-11, 13)),
-         "concDep_uncl_hillSlope")
-add_plot(plot_list,
-         create_boxplot(merged_data, "RMSE",
-                        "conc_dep", "unclear",
-                        ylim_range = c(0, 65)),
-         "concDep_uncl_rmse")
-add_plot(plot_list,
-         create_boxplot(merged_data, "MAE",
-                        "conc_dep", "unclear",
-                        ylim_range = c(0, 55)),
-         "concDep_uncl_mae")
-
-### conc_dep + critical
-# EC50
-add_plot(plot_list,
-         create_boxplot(data_V2, "EC50",
-                        "conc_dep", "critical",
-                        ylim_range = c(-0.5, 3)),
-         "concDep_crit_V2")
-# 16x values not displayed as EC50 > 3; all in conc_dep = FALSE
-add_plot(plot_list,
-         create_boxplot(data_b2, "EC50",
-                        "conc_dep", "critical",
-                        ylim_range = c(-0.5, 10)),
-         "concDep_crit_b2")
-# 8x values not displayed as EC50 > 10; all in conc_dep = FALSE
-add_plot(plot_list,
-         create_boxplot(data_V2Cterm, "EC50",
-                        "conc_dep", "critical",
-                        ylim_range = c(-0.5, 10)),
-         "concDep_crit_V2Cterm")
-add_plot(plot_list,
-         create_boxplot(data_b2Cterm, "EC50",
-                        "conc_dep", "critical",
-                        ylim_range = c(-0.5, 10)),
-         "concDep_crit_b2Cterm")
-
-
-# EC50 log transformed
-add_plot(plot_list,
-         create_boxplot(data_V2, "EC50",
-                        "conc_dep", "critical",
-                        ylim_range = c(-7, 10), log_transform = TRUE),
-         "concDep_crit_V2_log")
-# 3x values not displayed as log10(EC50) > 10; all in conc_dep = FALSE
-add_plot(plot_list,
-         create_boxplot(data_b2, "EC50",
-                        "conc_dep", "critical",
-                        ylim_range = c(-5, 5), log_transform = TRUE),
-         "concDep_crit_b2_log")
-# all values displayed
-add_plot(plot_list,
-         create_boxplot(data_V2Cterm, "EC50",
-                        "conc_dep", "critical",
-                        ylim_range = c(-7, 10), log_transform = TRUE),
-         "concDep_crit_V2Cterm_log")
-add_plot(plot_list,
-         create_boxplot(data_b2Cterm, "EC50",
-                        "conc_dep", "critical",
-                        ylim_range = c(-5, 5), log_transform = TRUE),
-         "concDep_crit_b2Cterm_log")
-
-
-# remaining parameters
-add_plot(plot_list,
-         create_boxplot(merged_data, "Hill_slope",
-                        "conc_dep", "critical",
-                        ylim_range = c(-11, 13)),
-         "concDep_crit_hillSlope")
-add_plot(plot_list,
-         create_boxplot(merged_data, "RMSE",
-                        "conc_dep", "critical",
-                        ylim_range = c(0, 65)),
-         "concDep_crit_rmse")
-add_plot(plot_list,
-         create_boxplot(merged_data, "MAE",
-                        "conc_dep", "critical",
-                        ylim_range = c(0, 55)),
-         "concDep_crit_mae")
-
 ### conc_dep single factor
-# EC50
-add_plot(plot_list,
-         create_boxplot(data_V2, "EC50",
-                        "conc_dep",
-                        ylim_range = c(-0.5, 3)),
-         "concDep_V2")
-add_plot(plot_list,
-         create_boxplot(data_b2, "EC50",
-                        "conc_dep",
-                        ylim_range = c(-0.5, 10)),
-         "concDep_b2")
-add_plot(plot_list,
-         create_boxplot(data_V2Cterm, "EC50",
-                        "conc_dep",
-                        ylim_range = c(-0.5, 10)),
-         "concDep_V2Cterm")
-add_plot(plot_list,
-         create_boxplot(data_b2Cterm, "EC50",
-                        "conc_dep",
-                        ylim_range = c(-0.5, 10)),
-         "concDep_b2Cterm")
-
 # EC50 log transformed
 add_plot(plot_list,
          create_boxplot(data_V2, "EC50",
@@ -340,16 +164,6 @@ add_plot(plot_list,
                         "conc_dep",
                         ylim_range = c(-5, 5), log_transform = TRUE),
          "concDep_b2_log")
-add_plot(plot_list,
-         create_boxplot(data_V2Cterm, "EC50",
-                        "conc_dep",
-                        ylim_range = c(-7, 10), log_transform = TRUE),
-         "concDep_V2Cterm_log")
-add_plot(plot_list,
-         create_boxplot(data_b2Cterm, "EC50",
-                        "conc_dep",
-                        ylim_range = c(-5, 5), log_transform = TRUE),
-         "concDep_b2Cterm_log")
 add_plot(plot_list,
          create_boxplot(merged_data, "EC50",
                         "GPCR", "conc_dep",
@@ -377,16 +191,6 @@ add_plot(plot_list,
                         "GPCR", "conc_dep",
                         ylim_range = c(-5, 3), log_transform = TRUE),
          "concDep_hillSlopeLog_GPCR")
-add_plot(plot_list,
-         create_boxplot(merged_data, "RMSE",
-                        "conc_dep",
-                        ylim_range = c(0, 65)),
-         "concDep_rmse")
-add_plot(plot_list,
-         create_boxplot(merged_data, "MAE",
-                        "conc_dep",
-                        ylim_range = c(0, 55)),
-         "concDep_mae")
 
 ### 2D plotting
 create_xy_plot <- function(data, x_col, y_col, factor1 = NULL, factor2 = NULL,
@@ -508,133 +312,54 @@ write_xlsx(merged_data[rows_HS_neg,], paste0(getwd(), r"(\categories\Neg_Hillslo
 rows_HS_critical <- which(merged_data$Hill_slope > 0.01 & merged_data$Hill_slope < 0.1)
 write_xlsx(merged_data[rows_HS_critical,], paste0(getwd(), r"(\categories\Critical_HillSlope.xlsx)"))
 
+### used 2D plots ###
+# 12 points not visible as log10(EC50) > 300
+add_plot(plot_list,
+         create_xy_plot(merged_data,
+                        "EC50", "absolute_Hill_slope",
+                        "conc_dep",
+                        x_range = c(-5, 300),
+                        log_x = TRUE,
+                        log_y = TRUE),
+         "logEC50_logabsHS_all_data")
 
 add_plot(plot_list,
          create_xy_plot(merged_data,
                         "EC50", "Hill_slope",
-                        "conc_dep", "critical",
-                        x_range = c(-5, 300),
-                        log_x = TRUE,
-                        label_below_y = -1),
-         "logEC50_HS_all_data_crit")
-add_plot(plot_list,
-         create_xy_plot(merged_data,
-                        "EC50", "Hill_slope",
-                        "conc_dep", "critical",
+                        "conc_dep",
                         x_range = c(-5, 300),
                         log_x = TRUE,
                         log_y = TRUE),
-         "logEC50_logHS_all_data_crit")
+         "logEC50_logHS_all_data")
+
 add_plot(plot_list,
          create_xy_plot(merged_data,
-                        "EC50", "Hill_slope",
-                        "conc_dep",
-                        x_range = c(-5, 300),
-                        log_x = TRUE),
-         "logEC50_HS_all_data")
-add_plot(plot_list,
-         create_xy_plot(merged_data,
-                        "EC50", "Hill_slope",
+                        "EC50", "absolute_Hill_slope",
                         "conc_dep",
                         x_range = c(-5, 300),
                         log_x = TRUE,
+                        log_y = TRUE,
                         label_below_y = 0.03,
+                        label_above_x = 3,
                         label_below_x = 0.0003),
-         "logEC50_HS_all_data_label")
-add_plot(plot_list,
-         create_xy_plot(merged_data,
-                        "EC50", "Hill_slope",
-                        "conc_dep",
-                        x_range = c(-5, 300),
-                        log_x = TRUE,
-                        log_y = TRUE),
-         "logEC50_logHS_all_data_cd")
-add_plot(plot_list,
-         create_xy_plot(merged_data,
-                        "EC50", "Hill_slope",
-                        "conc_dep",
-                        x_range = c(-5, 300),
-                        log_x = TRUE,
-                        log_y = TRUE,
-                        label_below_x = 0.001,
-                        label_above_x = 3,
-                        label_below_y = 0.01),
-         "logEC50_logHS_all_data_cd_label")
-add_plot(plot_list,
-         create_xy_plot(data_b2,
-                        "EC50", "Hill_slope",
-                        "conc_dep",
-                        x_range = c(-5, 300),
-                        log_x = TRUE,
-                        log_y = TRUE),
-         "logEC50_logHS_b2_cd")
-add_plot(plot_list,
-         create_xy_plot(data_b2,
-                        "EC50", "Hill_slope",
-                        "conc_dep",
-                        x_range = c(-5, 300),
-                        log_x = TRUE,
-                        log_y = TRUE,
-                        label_below_x = 0.001,
-                        label_below_y = 0.1),
-         "logEC50_logHS_b2_cd_label")
-add_plot(plot_list,
-         create_xy_plot(data_V2,
-                        "EC50", "Hill_slope",
-                        "conc_dep",
-                        x_range = c(-5, 300),
-                        log_x = TRUE,
-                        log_y = TRUE),
-         "logEC50_logHS_V2_cd")
-add_plot(plot_list,
-         create_xy_plot(data_V2,
-                        "EC50", "Hill_slope",
-                        "conc_dep",
-                        x_range = c(-5, 300),
-                        log_x = TRUE,
-                        log_y = TRUE,
-                        label_below_x = 0.0001,
-                        label_above_x = 3),
-         "logEC50_logHS_V2_cd_label")
-add_plot(plot_list,
-         create_xy_plot(data_b2Cterm,
-                        "EC50", "Hill_slope",
-                        "conc_dep",
-                        x_range = c(-5, 300),
-                        log_x = TRUE,
-                        log_y = TRUE),
-         "logEC50_logHS_b2Cterm_cd")
-add_plot(plot_list,
-         create_xy_plot(data_b2Cterm,
-                        "EC50", "Hill_slope",
-                        "conc_dep",
-                        x_range = c(-5, 300),
-                        log_x = TRUE,
-                        log_y = TRUE,
-                        label_below_x = 0.0001,
-                        label_above_x = 3,
-                        label_below_y = 0.04),
-         "logEC50_logHS_b2Cterm_cd_label")
-add_plot(plot_list,
-         create_xy_plot(data_V2Cterm,
-                        "EC50", "Hill_slope",
-                        "conc_dep",
-                        x_range = c(-5, 300),
-                        log_x = TRUE,
-                        log_y = TRUE),
-         "logEC50_logHS_V2Cterm_cd")
-add_plot(plot_list,
-         create_xy_plot(data_V2Cterm,
-                        "EC50", "Hill_slope",
-                        "conc_dep",
-                        x_range = c(-5, 300),
-                        log_x = TRUE,
-                        log_y = TRUE,
-                        label_above_x = 3,
-                        label_below_x = 0.001,
-                        label_below_y = 0.03),
-         "logEC50_logHS_V2Cterm_cd_label")
+         "logEC50_logabsHS_all_data_label")
 
+add_plot(plot_list,
+         create_xy_plot(data_b2,
+                        "EC50", "absolute_Hill_slope",
+                        "conc_dep",
+                        x_range = c(-5, 300),
+                        log_x = TRUE,
+                        log_y = TRUE),
+         "logEC50_logabsHS_b2_cd")
+add_plot(plot_list,
+         create_xy_plot(data_V2,
+                        "EC50", "absolute_Hill_slope",
+                        "conc_dep",
+                        x_range = c(-5, 300),
+                        log_x = TRUE,
+                        log_y = TRUE),
+         "logEC50_logabsHS_V2_cd")
 
 # export created plots
 setwd(paste0(getwd(), r"(\categories\)"))
